e718582da22d7afdbcb04eacf29296355e19d65d
QAU3 <cc.mijangos@gmail.com>
2023-12-11 19:16:47 +0100

    back-end updated for integration

--- back-end/index.js
+++ back-end/index.js
@@ -1,15 +1,29 @@
+// import dependencies and initialize the express app
 const express = require('express')
 require('dotenv').config()
 const app = express()
+const cors = require("cors")
+const path = require('path')
 const port = process.env.PORT || 5000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// import local files
 const novice = require('./routes/novice')
 const users = require('./routes/users')
+
+// configurations
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cors({
+  methods:["GET", "POST"],
+}))
+
+// actual routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html")) 
+})
 
 app.use('/novice', novice)
 app.use('/users', users)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// start the express server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -3,35 +3,41 @@ const users = express.Router();
 const DB=require('../db/dbConn.js')
 
 //Checks if user submited both fields, if user exist and if the combiation of user and password matches
+// TODO: implement  sessions and cookies to keep the user logged in.
+// TODO: implement a logout route 
+// TODO: use encryption to store passwords
+
 users.post('/login', async (req, res, next) => {
-   try{
-    const username = req.body.username;
-	const password = req.body.password;
-    if (username && password) {
-        const queryResult=await DB.AuthUser(username)
-        if(queryResult.length>0){
-            if(password===queryResult[0].user_password){
-                req.session.user=queryResult
-                console.log(req.session.user)
-                console.log(queryResult)
-                console.log("SESSION VALID")
-            } else{
-                 console.log("INCORRECT PASSWORD")
-                }
-            } else{
-                console.log("USER NOT REGISTRED");   
-            }
-        } 
-        else {
-            console.log("Please enter Username and Password!")
-        }
-    res.end();
-   }catch(err){
-    console.log(err)
-    res.sendStatus(500)
-    next()
-   }
-});
+    try{
+     const username = req.body.username;
+     const password = req.body.password;
+     if (username && password){
+         const queryResult=await DB.AuthUser(username)        
+         if(queryResult.length>0){
+             if(password===queryResult[0].user_password){
+                 console.log(queryResult)
+                 res.send({logged:true, user:queryResult[0]})
+             } else{
+                 res.sendStatus(204)
+                  console.log("INCORRECT PASSWORD")
+             }
+         } else{
+             res.sendStatus(204)
+             console.log("USER NOT REGISTRED");   
+         }
+     } 
+     else {
+         res.sendStatus(204)
+         console.log("Please enter Username and Password!")
+     }
+     res.end();
+    }catch(err){
+     console.log(err)
+     res.sendStatus(500)
+     next()
+    }
+  });
+  
 
 //Inserts a new user in our database id field are complete
 users.post('/register', async (req, res, next) => {
