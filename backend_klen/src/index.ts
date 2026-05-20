609aa5261e6073c334392b4dc06b4ff993678800
klen-copic <klen.copic@gmail.com>
2026-05-20 09:56:22 +0200

    solution

---
+++ backend_klen/src/index.ts
@@ -0,0 +1,32 @@
+import "dotenv/config";
+import express, { Request, Response, NextFunction } from "express";
+import newsRouter from "./routes/news.routes.js";
+import usersRouter from "./routes/users.routes.js";
+import cors from "cors";
+
+const app = express();
+const port = Number(process.env.PORT) || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (_req: Request, res: Response) => {
+  res.send("Hello from Express 5 and TypeScript");
+});
+
+app.use("/news", newsRouter);
+app.use("/users", usersRouter);
+
+app.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(error);
+
+  res.status(500).json({
+    success: false,
+    message: "Internal server error",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
